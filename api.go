a3eb5906e146018bf27654a2c1fe97f067797c3b
Josef May <josefjmay@gmail.com>
2024-03-18 22:05:07 -0400

    can listen to http requests

--- api.go
+++ api.go
@@ -1,12 +1,26 @@
 package main
 
+import (
+	"fmt"
+	
+	"net/http"
+)
+
 type APIServer struct {
 	listenAddr string
 }
 
-func newAPIServer(listenAddr sting) * APIServer{
+func newAPIServer(listenAddr string) * APIServer{
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
+}
+
+func (s *APIServer) Run() {
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Welcome to my website!")
+    })
+
+	http.ListenAndServe(s.listenAddr, nil)
 }
 
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
+
 import (
 	"fmt"
 )
+
 func main() {
-	fmt.Println("yeah buddy!")
+	server := newAPIServer(":3000")
+	fmt.Println("server is running on port ", server.listenAddr)
+	server.Run()
 }
